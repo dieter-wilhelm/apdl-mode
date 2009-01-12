ba5f062c0cf614b0427cdd5d377f5f41c691f891
dieter.wilhelm <dieter.wilhelm@d5a51148-9f23-0410-882a-37b6fa6ce781>
2009-01-12 14:45:39 +0000

    updated default.el for working place

--- branches/11.0.1/default.el
+++ branches/11.0.1/default.el
@@ -1,4 +1,5 @@
-;; default.el for Ansys mode under Unix and Windows
+;; example `default.el' for Ansys mode under Unix and Windows
+
 (autoload 'ansys-mode "ansys-mode" "Activate Ansys mode." 'interactive)
 (autoload 'ansys-display-error-file "ansys-mode" "Activate Ansys display error file function." 'interactive)
 (autoload 'ansys-start-ansys-help "ansys-mode" "Activate Ansys start help function." 'interactive)
@@ -6,18 +7,21 @@
 
 (add-to-list 'auto-mode-alist '("\\.mac$" . ansys-mode))
 (add-to-list 'auto-mode-alist '("\\.inp$" . ansys-mode))
+(add-to-list 'auto-mode-alist '("\\.anf$" . ansys-mode))
 
 ;; processes stuff
-(setq ansys-license-file "27005@rbgs421x")
+(setq ansys-license-file "27005@lic-rbg1")
 (if (string= window-system "x")
     (progn
       (setq  ansys-program "/appl/ansys_inc/v110/ansys/bin/ansys110")
       (setq ansys-lmutil-program "/appl/ansys_inc/shared_files/licensing/linop64/lmutil")
       (setq ansys-help-file "/appl/ansys_inc/v110/ansys/bin/anshelp110"))
   (progn
-    (setq ansys-lmutil-program "c:\\Program Files\\Ansys Inc\\Shared Files\\licensing\\intel\\anslic_admin.exe")
-					;'\"' on windows mandatorya
+    (setq ansys-lmutil-program "C:\\Program Files\\Ansys Inc\\Shared Files\\licensing\\intel\\anslic_admin.exe")
+					;'\"' on windows mandatory
     (setq ansys-help-file "\"C:\\Program Files\\Ansys Inc\\v110\\CommonFiles\\HELP\\en-us\\ansyshelp.chm\"")))
+
+(setq ansys-licence "preppost")
 
 ;; insertion stuff
 (auto-insert-mode 1)

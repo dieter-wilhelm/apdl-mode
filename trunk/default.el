67b13e2ef7604a7a147ef8bf8fce172bab7befd3
dieter.wilhelm <dieter.wilhelm@d5a51148-9f23-0410-882a-37b6fa6ce781>
2009-11-26 14:02:12 +0000

    updated default.el for 12.0.1

--- trunk/default.el
+++ trunk/default.el
@@ -1,12 +1,17 @@
 ;; example `default.el' for Ansys mode under Unix and Windows
 ;; `default.el' can be used as a system wide startup file.  E. g. in
-;; `/usr/local/share/emacs/site-lisp' or `INSTALLDIR/site-lisp'
+;; `/usr/local/share/emacs/site-lisp' or `EMACS_INSTALLDIR/site-lisp'
+
+(add-to-list 'load-path "C:\\DIRECTORY-PATH\\WHERE\\THE\\ANSYS-MODE\\FILES\\RESIDE")
+      ;assuming you extracted the files on drive "c:"
+      ;for example: "c:\\emacs\\emacs-23.1"
 
 (autoload 'ansys-mode "ansys-mode" "Activate Ansys mode." 'interactive)
 (autoload 'ansys-display-error-file "ansys-mode" "Activate Ansys display error file function." 'interactive)
 (autoload 'ansys-start-ansys-help "ansys-mode" "Activate Ansys start help function." 'interactive)
 (autoload 'ansys-license-status "ansys-mode" "Activate Ansys license status function." 'interactive)
 
+;; file suffixes for which Ansys mode is automatically called
 (add-to-list 'auto-mode-alist '("\\.mac$" . ansys-mode))
 (add-to-list 'auto-mode-alist '("\\.inp$" . ansys-mode))
 (add-to-list 'auto-mode-alist '("\\.anf$" . ansys-mode))
@@ -15,21 +20,24 @@
 (setq ansys-license-file "27005@lic-rbg1")
 (if (string= window-system "x")
     (progn
-      (setq  ansys-program "/appl/ansys_inc/v110/ansys/bin/ansys110")
+      ;; The Ansys executable
+      (setq  ansys-program "/appl/ansys_inc/v120/ansys/bin/ansys120")
+      ;; Tool for the license status
       (setq ansys-lmutil-program "/appl/ansys_inc/shared_files/licensing/linop64/lmutil")
-      (setq ansys-help-file "/appl/ansys_inc/v110/ansys/bin/anshelp110"))
+      ;; Ansys help browser executable
+      (setq ansys-help-file "/appl/ansys_inc/v120/ansys/bin/anshelp120"))
   (progn
     (setq ansys-lmutil-program "C:\\Program Files\\Ansys Inc\\Shared Files\\licensing\\intel\\anslic_admin.exe")
-					;'\"' on windows mandatory
-    (setq ansys-help-file "\"C:\\Program Files\\Ansys Inc\\v110\\CommonFiles\\HELP\\en-us\\ansyshelp.chm\"")))
+					;backslash '\"' on windows mandatory
+    (setq ansys-help-file "\"C:\\Program Files\\Ansys Inc\\v120\\CommonFiles\\HELP\\en-us\\ansyshelp.chm\"")))
 
-(setq ansys-licence "preppost")
+(setq ansys-licence "struct")	     ;which license to use for the run
 
-;; insertion stuff
+;; auto insertion stuff (when creating an APDL file)
 (auto-insert-mode 1)
 (add-hook 'find-file-hook 'auto-insert)
 (setq auto-insert-query t)
-(add-to-list 'auto-insert-alist '(ansys-mode . [ansys-skeleton]))
+(add-to-list 'auto-insert-alist '(ansys-mode . [ansys-skeleton-compilation]))
 
 ;; experimental user variables highlighting
 ;(setq ansys-dynamic-highlighting-flag t)

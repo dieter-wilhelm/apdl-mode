eb1123b315b7ee97e05de12fc4a9db397013739a
dieter.wilhelm <dieter.wilhelm@d5a51148-9f23-0410-882a-37b6fa6ce781>
2010-01-06 13:00:37 +0000

    squashed buf in -display-skeleton

--- trunk/ansys-template.el
+++ trunk/ansys-template.el
@@ -14,7 +14,7 @@
 				t "ansys-skeleton-" nil)))
     (save-excursion
       (set-buffer skeleton-buffer)
-      ;; (make-local-variable 'ansys-skeleton-overlay)
+      (make-local-variable 'ansys-skeleton-overlay)
       (remove-overlays)
       (setq ansys-skeleton-overlay (make-overlay 1 1))
       ;; TODO: remove possible command-help overlays!
@@ -232,7 +232,7 @@
   "!! --- Contact Options --"\n
   "keyo,Contact,2,1 !ALGORITHM [0]:augm. Lagrange,1:penalty,2:MPC,4:pure Lagrange" \n
   "!! " \n
-  "Fkn = .1 !contact stiffness (default 1, divided by 100 if plastic mat. < AnsysWB12)" \n
+  "Fkn = .1 !contact stiffness (default 1, divided by 100 if plastic mat. < Ansys 12.0)" \n
   "rmod,Contact,3,Fkn !FKN:normal penalty stiffness factor (default:1) smaller: bigger penetration, easier convergence" \n
   "!rmod,Contact,12,0. !FKT:tangent stiffness factor,0:means 1 for Ansys!!!" \n
   \n

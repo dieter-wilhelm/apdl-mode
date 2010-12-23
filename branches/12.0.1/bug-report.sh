b576053f65e12827cd4f5462ba5986f531f2c48e
dieter.wilhelm <dieter.wilhelm@d5a51148-9f23-0410-882a-37b6fa6ce781>
2010-12-23 13:00:52 +0000

    overwriting didn't work for branches/12.0.1 deleting it

--- branches/12.0.1/bug-report.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#returns any defcustom variable from ansys-mode.el in quoted form
-# for creating a list in ansys-submit-bug-report
-
-cat ansys-mode.el | sed -n -e  "s/^(defcustom/ /p"| \
-    sed -n -e "s/  \(\(\w*-*\)*\).*/'\1/p"

671551af34f25cd6a48d4bae8d88450d9d4032cb
dieter-wilhelm <dieter@duenenhof-wilhelm.de>
2015-07-09 16:15:35 +0200

    new structure WIP

--- helper/default.el
+++ helper/default.el
@@ -1,7 +1,2 @@
-;;; default.el - loaded after ".emacs" on startup
-;;;
-;;; Setting `inhibit-default-init' non-nil in "~/.emacs"
-;;; prevents loading of this file.  Also the "-q" option to emacs
-;;; prevents both "~/.emacs" and this file from being loaded at startup.
-
-(load-file "ansys-mode-16.1.1\\a-m")
+This is an autmatically created file
+(load-file "ansys-mode-16.1.test2\\a-m")

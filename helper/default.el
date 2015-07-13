cd2ff1f6d26bf9140fd601a7728faf97f3006625
dieter-wilhelm <dieter@duenenhof-wilhelm.de>
2015-07-13 10:56:35 +0200

    automated A-M.sh creation

--- helper/default.el
+++ helper/default.el
@@ -1,3 +1,3 @@
 ;;; Automatically created from ANSYS-mode-16.1.test2
 (let ((dir (file-name-directory load-file-name)))
- (load-file (concat dir "\\ansys-mode-16.1.test2\\A-M.el"))
+ (load-file (concat dir "\\\\ansys-mode-16.1.test2\\\\A-M.el"))
--- helper/tools/bin/ansys-mode.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-emacs -l /appl/tools/ansys-mode/ansys-mode-16.1.1/a-m.el $*

eca816de0ce6e9a8eb276d73aa9655e94be8b814
dieter-wilhelm <uidg1626@vitesco.com>
2025-05-02 20:46:36 +0200

    feat: Add undocumented commands shown in .parm files
    created with PARSAV

--- helper/fontification.el
+++ helper/fontification.el
@@ -1,7 +1,6 @@
 ;;; fontification.el --- building keywords and completions
-;; Time-stamp: <2021-10-02>
 
-;; Copyright (C) 2006 - 2021 H. Dieter Wilhelm
+;; Copyright (C) 2006 - 2025 H. Dieter Wilhelm
 
 ;; Author: H. Dieter Wilhelm <dieter@duenenhof-wilhelm.de>
 ;; Version: 20.7.0
@@ -110,6 +109,8 @@
 
 (defconst Apdl_undocumented_commands    ; or macros?
   '(
+    "*PREAD"			   ;in .parm file from PARSAV
+    "END"			   ;in .parm file
     "/WB"                     ; signify a WB generated input file
     "XMLO"
     "/XML"
